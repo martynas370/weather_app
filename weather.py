517b78ea7d2574d7cd8baf46273de10ec64252bf
Martynas <64475562+martynas370@users.noreply.github.com>
2021-04-23 19:24:59 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,9 +1,11 @@
 import ssl, decimal
 import urllib.request, urllib.parse, urllib.error
 import json
+
 #metaweather location url
 link='https://www.metaweather.com/api/location/search/?'
 link2='https://www.metaweather.com/api/location/'
+
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -11,33 +13,32 @@ ctx.verify_mode = ssl.CERT_NONE
 
 #enter location
 while True:
-    inpu=input('Enter your location: ')
-    if len(inpu)<1:
-        print('no place')
+    enter_location=input('Enter your location: ')
+    if len(enter_location)<1:
+        print('No location found')
         break
-    inp=inpu.replace(' ','+') #replace whitespace with +
+    inp=enter_location.replace(' ','+') #replace whitespace with +
     url=link+'query='+inp
+    
 #send request,open data
     hand = urllib.request.urlopen(url, context=ctx)
     data = hand.read().decode()
+    
 #check if data in order
     try:
         js=json.loads(data)
     except:
         js=None
-    #print(json.dumps(js, indent=4))
+    
     if len(js)<1:
-        print('Location <',inp,'> not found.')
+        print('Location <',inp,'> not found')
         break
-    else:
-        #print('City ',js[0]['title'])
-        #print('Where on earth id ', js[0]['woeid'])
-        #print('Latt','Long',js[0]['latt_long'])
 
 #request detailed data with woeid
         url2=link2+str(js[0]['woeid'])
         hand2=urllib.request.urlopen(url2, context=ctx)
         data2=hand2.read().decode()
+        
         try:
             js2=json.loads(data2)
         except:
@@ -46,6 +47,6 @@ while True:
             print('No data found.')
             break
         else:
-            #print(json.dumps(js2, indent=4))
             print('Temperature in ',inpu,': ',round(js2['consolidated_weather'][0]['the_temp']), 'degrees C')
+            
         quit()
